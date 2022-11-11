cede887e4e1dc995d95ccab94ef33e00141092d0
YeYusuf <yemi.yusuf@barclays.com>
2022-11-11 16:47:10 +0000

    second commit

--- routers/shows.js
+++ routers/shows.js
@@ -1,8 +1,32 @@
 const { Router } = require ("express")
 const showRouter = Router();
+const Show = require ("../models/Show")
 
-showRouter.get ('/',(req,res) =>{
-    res.sendStatus(200);
+
+showRouter.get ('/',async(req,res) =>{
+    try {
+        const showOne = await Show.findByPk(req.params.id);
+        res.send(showOne)
+    } catch (error) {
+        res.send(error)
+    }
 })
+
+
+showRouter.get ('/',async(req,res) =>{
+    try {
+        const showOne = await Show.findByPk(req.params.id);
+        res.send(showOne)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
+
+
+
+
+
 
 module.exports = showRouter
--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,8 @@
 const { Router } = require ("express")
 const userRouter = Router();
 const User = require ("../models/User")
+
+
 userRouter.get ('/',(req,res) =>{
     res.sendStatus(200);
 });
@@ -11,5 +13,24 @@ userRouter.post("/",async(req,res)=>{
         res.status(500).send(error)
     }
 })
+
+userRouter.get("/users/:id",async(req,res)=>{
+    try {
+        const userOne = await User.findByPk(req.params.id);
+        res.send(userOne)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+userRouter.get("/",async(req,res)=>{
+    try {
+        const userAll = await User.findAll();
+        res.send(userAll)
+    } catch (error) {
+        res.send(error)}
+    })
+
+
 
 module.exports = userRouter
